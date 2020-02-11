a2e21016e99e24bd7cb55e69589d4f6ac2942629
Javier Garcia <javier.garcia@voltanet.io>
2020-02-11 17:34:04 +0100

    Setting up clang-tidy alternative instalation

--- reports/analyze.sh
+++ reports/analyze.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
+echo "##########################################"
+echo "### START Running Analysis    ############"
+echo "##########################################"
+#set -x#Uncomment to debug
 make clean
 bear make
-REPORT=reports/clang-tidy-report
+REPORT=./reports/clang-tidy-report
 >$REPORT
 echo "Report file is : [$REPORT]"
-echo "Running clang-tidy : $(clang-tidy --version)"
-clang_version=$(clang-tidy --version|grep version|awk '{print $3}')
-if [[ $clang_version =~ ^"9.0" ]];
+echo "Guessing which clang-tidy version we have"
+which clang-tidy >/dev/null && export CLANG_TIDY_PROGRAM=clang-tidy
+which clang-tidy-7 >/dev/null && export CLANG_TIDY_PROGRAM=clang-tidy-7
+which clang-tidy-8 >/dev/null && export CLANG_TIDY_PROGRAM=clang-tidy-8
+which clang-tidy-9 >/dev/null && export CLANG_TIDY_PROGRAM=clang-tidy-9
+if [ ! -z $CLANG_TIDY_PROGRAM ]
 then
-    echo "New version most checks, I'm happy"
-    find . ! -name "*test*" \( -name "*.c"  \)  \( -exec echo FILE_NAME == {} \; -exec clang-tidy   -p .  -checks="$(cat clang-tidy-checks.txt)" {}  \;   \) >> $REPORT
+    echo "Running clang-tidy : $( $CLANG_TIDY_PROGRAM --version)"
+    clang_version=$($CLANG_TIDY_PROGRAM --version|grep version|awk '{print $3}'|awk -F. '{print $1}')
+    echo "Version found $clang_version"
+    if [ $(($clang_version)) -le 6 ]
+    then
+        echo "Old version no checks, version 3 it's unstable"
+        find . ! -name "*test*" \( -name "*.c"  \)  -exec $CLANG_TIDY_PROGRAM   -p .  -checks="$(cat clang-tidy-checks_3.txt)" {}  \;    >> $REPORT
+    else
+        if [ $clang_version -ge 7 ]
+        then
+            echo "New version most checks, I'm happy"
+            find . ! -name "*test*" \( -name "*.c"  \)  -exec $CLANG_TIDY_PROGRAM   -p .  -checks="$(cat clang-tidy-checks.txt)" {} -- $(cat .clang_tidy)  \;  >> $REPORT
+        fi
+    fi
 else
-    echo "Old version no checks, it's instable"
-    find . ! -name "*test*" \( -name "*.c"  \)  \( -exec echo FILE_NAME == {} \; -exec clang-tidy   -p .  -checks="$(cat clang-tidy-checks_3.txt)" {}  \;   \) >> $REPORT
+    echo "##########################################"
+    echo "### NO clang-tidy installed ##############"
+    echo "##########################################"
 fi
-num_warnings=$(grep "warning:" $REPORT |wc -l)
-num_error=$(grep "error:" $REPORT |wc -l)
+
+
+
+
+num_warnings=$(grep -c "warning:" $REPORT )
+num_error=$(grep -c "error:" $REPORT )
 ls -l $REPORT
 echo "Report file $REPORT with : warning [$num_warnings] and error [$num_error] "
-#for file in $(find . ! -name "*test*" \( -name "*.c"  \) );
-#do
-#    echo "Analyze file :[$file]"
-#    clang-tidy  -p .  -checks="$(cat clang-tidy-checks.txt)" "$file" >> $REPORT;
-#done
+echo "##########################################"
+echo "### END Running Analysis    ##############"
+echo "##########################################"
--- reports/install.sh
+++ reports/install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-sudo apt update
-sudo apt install bear clang-tidy -y
+#set -x
+echo "##########################################"
+echo "### START Running Install   ##############"
+echo "##########################################"
+#sudo echo 'deb http://deb.debian.org/debian stretch-backports main' >> /etc/apt/sources.list.d/backports.list
+#sudo apt update
+#sudo apt install bear -y
+#sudo apt list clang-tidy-7
+#sudo apt install clang-tidy-7 -y -t stretch-backports
+echo "##########################################"
+echo "### END   Running Install   ##############"
+echo "##########################################"
